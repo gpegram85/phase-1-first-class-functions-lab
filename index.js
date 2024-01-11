2d61a26f0b6536eb6c2965d96af5d67ece2b6303
Grey Pegram <dpegram85@gmail.com>
2024-01-10 23:06:36 -0500

    Completed Labs

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    const firstTwo = drivers.slice(0, 2)
+    return firstTwo    
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    const lastTwo = drivers.slice(-2)
+    return lastTwo
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fare) {
+    return function fareMultiplier(multiplier) {
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(fare)(2)
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(fare)(3)
+}
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers)
+}
